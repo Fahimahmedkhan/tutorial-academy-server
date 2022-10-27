e17820f6038a8e7d72d57e4398675572f1d13662
fahimahmedkhan <rolx9999@gmail.com>
2022-10-27 06:25:52 +0600

    tutorial

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 
 const tutorialCategories = require('./data/tutorialCategories.json');
+const tutorial = require('./data/tutorial.json');
 
 app.get('/', (req, res) => {
     res.send('Tutorial Academy Server is running')
@@ -18,8 +19,23 @@ app.get('/tutorialCategories', (req, res) => {
 
 app.get('/tutorialCategories/:id', (req, res) => {
     const id = req.params.id;
-    const tutorialCategory = tutorialCategories.find(tutorialCategory => tutorialCategory.id == id)
-    res.send(tutorialCategory);
+    if (id === '09') {
+        res.send(tutorial);
+    }
+    else {
+        const tutorial_categories = tutorial.filter(a => a.category_id === id);
+        res.send(tutorial_categories);
+    }
+});
+
+app.get('/tutorial', (req, res) => {
+    res.send(tutorial);
+});
+
+app.get('/tutorial/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedTutorial = tutorial.find(a => a._id === id);
+    res.send(selectedTutorial);
 });
 
 app.listen(port, () => {
